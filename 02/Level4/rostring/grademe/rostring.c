acb025301180ebf409f243b92d3480ac425e8e07
Marta en Mac <mvigara79@gmail.com>
2025-05-08 08:28:15 +0200

    fixed str_capitalizer tests

--- 02/Level4/rostring/grademe/rostring.c
+++ 02/Level4/rostring/grademe/rostring.c
@@ -1,67 +1,45 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-int ft_strlen(char *s)
+int is_space(char c)
 {
-    int i = 0;
-    while (s[i])
+    return(c == ' '|| c == '\t');
+}
+
+void rostring(char *s)
+{
+    int i = 0, end = 0, start = 0, fend = 0, fstart = 0;
+
+    while(s[i] && is_space(s[i]))
         i++;
-    return i;
+    fstart = i;
+    while(s[i] &&  !is_space(s[i]))
+        i++;
+    fend = i;
+
+    while(s[i])
+    {
+        while(s[i] && is_space(s[i]))
+            i++;
+        start = i;
+        while(s[i] && !is_space(s[i]))
+            i++;
+        end = i;
+        if(start < end)
+        {
+            write(1, s + start, end - start);
+            write(1, " ", 1);
+        }
+    }
+   
+    write(1, s + fstart, fend - fstart);
+
 }
 
 int main(int argc, char **argv)
 {
-    if (argc < 2)
-    {
-        write(1, "\n", 1);
-        return 0;
-    }
-    char *s = argv[1];
-    char *p = malloc(ft_strlen(s) + 1); // Buffer para la primera palabra
-    if (!p)
-        return 1;
-
-    int i = 0, j = 0;
-
-    // Saltar espacios iniciales
-    while (s[i] == ' ' || s[i] == '\t')
-        i++;
-
-    // Guardar la primera palabra
-    while (s[i] && s[i] != ' ' && s[i] != '\t')
-        p[j++] = s[i++];
-    p[j] = '\0';
-
-    // Saltar espacios después de la primera palabra
-    while (s[i] == ' ' || s[i] == '\t')
-        i++;
-
-    int first = 1;
-    // Imprimir el resto de las palabras
-    while (s[i])
-    {
-        // Saltar espacios antes de la palabra
-        while (s[i] == ' ' || s[i] == '\t')
-            i++;
-        if (!s[i])
-            break;
-        if (!first)
-            write(1, " ", 1);
-        first = 0;
-        int start = i;
-        while (s[i] && s[i] != ' ' && s[i] != '\t')
-            i++;
-        write(1, &s[start], i - start);
-    }
-
-    // Imprimir la primera palabra al final
-    if (j > 0)
-    {
-        if (first == 0) // Si ya imprimiste palabras antes
-            write(1, " ", 1);
-        write(1, p, j);
-    }
+    if(argc >= 2)
+        rostring(argv[1]);
     write(1, "\n", 1);
-    free(p);
     return 0;
 }
