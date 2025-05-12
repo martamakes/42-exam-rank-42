3c6679058478b86e13498e0d9a5f0528b5430431
Marta en Mac <mvigara79@gmail.com>
2025-05-12 20:22:24 +0200

    easier version

--- 02/Level3/str_capitalizer/grademe/str_capitalizer.c
+++ 02/Level3/str_capitalizer/grademe/str_capitalizer.c
@@ -1,38 +1,62 @@
 #include <unistd.h>
-
-int is_word_start(int i, char *str)
+char to_lower(char c)
 {
-    return (i == 0 || str[i - 1] == ' ' || str[i - 1] == '\t');
+    if(c >= 'A' && c <= 'Z')
+        return(c += 32);
+    return c;
+}
+int is_upper(char c)
+{
+    return(c >= 'A' && c <= 'Z');
 }
 
-void str_capitalizer(char *str)
+char to_upper(char c)
 {
-    int i = 0;
-    
-    while (str[i])
+    if(c >= 'a' && c <= 'z')
+        return(c -= 32);
+    return c;
+}
+
+int is_space(char c)
+{
+    return(c == ' ' || c == '\t');
+}
+
+void capi(char *s)
+{
+    int i;
+    for(i = 0; s[i]; i++)
     {
-        if (str[i] >= 'A' && str[i] <= 'Z')
-            str[i] += 32; // Convertir a minúscula
-        
-        if (is_word_start(i, str) && str[i] >= 'a' && str[i] <= 'z')
-            str[i] -= 32; // Convertir a mayúscula si es inicio de palabra
-        
-        write(1, &str[i], 1);
-        i++;
+        if(is_upper(s[i]))
+            s[i] = to_lower(s[i]);
     }
-}
-
-int main(int argc, char *argv[])
-{
-    if (argc < 2)
-        write(1, "\n", 1);
-    else
+    if(s[0] && !is_space(s[0]))
     {
-        for (int i = 1; i < argc; i++)
+        s[0] = to_upper(s[0]);
+    }
+    for(i = 0; s[i]; i++)
+    {
+        
+        if(!is_space(s[i]) && is_space(s[i - 1]))
         {
-            str_capitalizer(argv[i]);
+            s[i] = to_upper(s[i]);
+        }
+        write(1, &s[i], 1);
+    }
+
+}
+int main(int argc, char **argv)
+{
+    if(argc >= 2)
+    {
+        for(int i = 1; argv[i]; i++)
+        {
+            capi(argv[i]);
             write(1, "\n", 1);
         }
+        
     }
-    return (0);
+    else
+        write(1, "\n", 1);
+    return 0;
 }
