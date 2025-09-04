7674e0c764435b21218d19f871b8f8f6823e8465
Marta Vigara <108988437+martamakes@users.noreply.github.com>
2025-09-04 19:52:45 +0200

    New Subjects for Level 1 Exam Rank 05 complete

--- new-exams/exam-rank-05/level-1/bigint/bigint.hpp
+++ new-exams/exam-rank-05/level-1/bigint/bigint.hpp
@@ -1,63 +1,69 @@
-#include <string>
-#include <iostream>
-#include <ostream>
-#include <sstream>  // Required for stringstream
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   bigint.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fatkeski <fatkeski@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/23 14:17:26 by fatkeski          #+#    #+#             */
+/*   Updated: 2025/08/01 13:57:18 by fatkeski         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-class	bigint
+#ifndef BIGINT
+#define BIGINT
+
+#include <sstream>
+#include <iostream>
+#include <string>
+#include <cstdlib>
+
+class bigint
 {
 	private:
-		std::string _string;
-
+		std::string str;
+		//std::string result;
 	public:
-		// Constructors
-		bigint();                    // defaults to 0
-		bigint(unsigned int n);     // constructs from int
-		bigint(const std::string& str_argument); // constructs from string
-		bigint(const bigint& original);   // copy constructor
-		~bigint();
+		bigint();
+		bigint(unsigned int num);
+		bigint(const bigint& source);
 
-		//getter function
-		std::string	getArbiter_value() const;
-		//addtion
-			// +
-			bigint	operator+(const bigint& other) const;
-			bigint	operator+(unsigned int value) const;
-			// +=
-			bigint	operator+=(const bigint& other);
-			bigint	operator+=(unsigned int value);
-			// ++
-			bigint	operator++(int);
-			bigint	operator++(void);
-		//comparison
-			// >
-			bool	operator>(const bigint& other);
-			bool	operator>(unsigned int value);
-			// <
-			bool	operator<(const bigint& other);
-			bool	operator<(unsigned int value);
-			// << -digitshift-
-			bigint	operator<<(unsigned int shift) const;
-			// >> -digitshift-
-			bigint	operator>>(unsigned int shift) const;
-			// <<= -digitshift-
-			bigint operator<<=(unsigned int shift);
-			// >>= -digitshift-
-			bigint operator>>=(unsigned int shift);
-			// <=
-			bool	operator<=(const bigint& other);
-			bool	operator<=(unsigned int shift);
-			// >=
-			bool	operator>=(const bigint& other);
-			bool	operator>=(unsigned int shift);
+		std::string getStr()const;
 
-			// operator=()// Copy Assignment Operator
-			bigint&	operator=(const bigint& other);
-			// ==
-			bool	operator==(const bigint& other);
-			bool	operator==(unsigned int shift);
-			// !=
-			bool	operator!=(const bigint& other);
-			bool	operator!=(unsigned int shift);
+		bigint& operator=(const bigint& source);
+
+		// addition
+		bigint operator+(const bigint& other)const;
+		bigint& operator+=(const bigint& other);
+
+		// increments
+		bigint& operator++(); // ++x
+		bigint operator++(int); // x++
+
+		// shift with num
+		bigint operator<<(unsigned int n)const;
+		bigint operator>>(unsigned int n)const;
+		bigint& operator<<=(unsigned int n);
+		bigint& operator>>=(unsigned int n);
+
+		//shift with object
+		bigint operator<<(const bigint& other)const;
+		bigint operator>>(const bigint& other)const;
+		bigint& operator<<=(const bigint& other);
+		bigint& operator>>=(const bigint& other);
+
+		// ==, !=, <, >, <=, >=
+		bool operator==(const bigint& other) const;
+		bool operator!=(const bigint& other) const;
+		bool operator<(const bigint& other) const;
+		bool operator>(const bigint& other) const;
+		bool operator<=(const bigint& other) const;
+		bool operator>=(const bigint& other) const;
+
+		// ~bigint();
+
 };
 
-std::ostream& operator<<(std::ostream& out, const bigint& value);
+std::ostream& operator<<(std::ostream& output, const bigint& obj);
+
+#endif
--- new-exams/exam-rank-05/level-1/vect2/main.cpp
+++ new-exams/exam-rank-05/level-1/vect2/main.cpp
@@ -1,39 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fatkeski <fatkeski@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/28 21:04:35 by fatkeski          #+#    #+#             */
+/*   Updated: 2025/07/29 15:21:50 by fatkeski         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "vect2.hpp"
 
-//clear && c++ main.cpp vect2.cpp vect2.hpp && valgrind --leak-check=full --show-leak-kinds=all ./a.out
-int	main(void)
+int main()
 {
-	vect2		v1;
-	vect2		v2(1, 2);
-	const vect2	v3(v2);
-	vect2		v4 = v2;
+	vect2 v1; // 0, 0
+	vect2 v2(1, 2); // 1, 2
+	const vect2 v3(v2); // 1, 2
+	vect2 v4 = v2; // 1, 2
 
-	std::cout << "v2 = " << v2 << std::endl;
-	std::cout << "v4++ = " << v4++ << std::endl;// 2, 3
-	std::cout << "++v4 = " << ++v4 << std::endl;// 3, 4
-	std::cout << "v4-- = " << v4-- << std::endl;// 2, 3
-	std::cout << "v4-- = " << --v4 << std::endl;// 1, 2
+	std::cout << "v1: " << v1 << std::endl;
+	std::cout << "v1: " << "{" << v1[0] << ", " << v1[1] << "}" << std::endl;
+	std::cout << "v2: " << v2 << std::endl;
+	std::cout << "v3: " << v3 << std::endl;
+	std::cout << "v4: " << v4 << std::endl;
+	std::cout << v4++ << std::endl; // 2, 3
+	std::cout << ++v4 << std::endl; // 3, 4
+	std::cout << v4-- << std::endl; // 2, 3
+	std::cout << --v4 << std::endl; // 1, 2
 
-	std::cout << "[Personal Test] v1 == v1-> " << (v1 == v1) << std::endl;
-	std::cout << "[Personal Test] v1 != v2-> " << (v1 != v2) << std::endl;
-	std::cout << "[Personal Test] v1 != v1-> " << (v1 != v1) << std::endl;
 
-	v2 += v3;
-	std::cout << "v2 += v3-> " << v2 << std::endl;// 2, 4
+	v2 += v3; // 2, 4
+	v1 -= v2; // -2, -4
+	v2 = v3 + v3 *2; // 3, 6
+	v2 = 3 * v2; // 9, 18
+	v2 += v2 += v3; // 20, 40
+	v1 *= 42; // -84, -168
+	v1 = v1 - v1 + v1;
 
-	(v1 -= v2);
-	std::cout << "v1 -= v2-> " << v1 << std::endl;// -2, -4
+	std::cout << "v1: " << v1 << std::endl;
+	std::cout << "v2: " << v2 << std::endl;
+	std::cout << "-v2: " << -v2 << std::endl;
+	std::cout << "v1[1]: " << v1[1] << std::endl;
+	v1[1] = 12;
+	std::cout << "v1[1]: " << v1[1] << std::endl;
+	std::cout << "v3[1]: " << v3[1] << std::endl;
+	std::cout << "v1 == v3: " << (v1 == v3) << std::endl;
+	std::cout << "v1 == v1: " << (v1 == v1) << std::endl;
+	std::cout << "v1 != v3: " << (v1 != v3) << std::endl;
+	std::cout << "v1 != v1: " << (v1 != v1) << std::endl;
 
-	v2 = v3 + v3;
-	std::cout << "v2 = v3 + v3-> " << v2 << std::endl;// 3, 6
-
-	v2 = 3 * v2;
-	std::cout << "v2 = 3 * v2-> " << v2 << std::endl;// 9, 18
-
-	v2 += v2 += v3;
-	std::cout << "v2 += v2 += v3> " << v2 << std::endl;// 20, 40
-
-	std::cout << "v1 = v3-> " << (v1 == v3) << std::endl;
-	std::cout << "v1 == v1-> " << (v1 == v1) << std::endl;
-	std::cout << "v1 != v3-> " << (v1 != v3) << std::endl;
+	return (0);
 }
