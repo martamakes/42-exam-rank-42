eff3abce2a8d52f7baee203e5b39391cff8bad0d
Marta en Mac <mvigara79@gmail.com>
2026-02-11 17:38:08 +0100

    argo y vbc

---
+++ rendu4/argo/argo.h
@@ -0,0 +1,35 @@
+#ifndef ARGO_H
+# define ARGO_H
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
+
+typedef struct	json {
+	enum {
+		MAP,
+		INTEGER,
+		STRING
+	} type;
+	union {
+		struct {
+			struct pair	*data;
+			size_t		size;
+		} map;
+		int		integer;
+		char	*string;
+	};
+}	json;
+
+typedef struct	pair {
+	char	*key;
+	json	value;
+}	pair;
+
+int		argo(json *dst, FILE *stream);
+void	free_json(json j);
+void	serialize(json j);
+
+#endif
