a8d8459226b6772fc4d2575d01011dbc5c73be94
Marta en Mac <mvigara79@gmail.com>
2026-04-05 18:02:47 +0200

    mejoras implementación vbc

--- rendu4/vbc/vbc2/vbc.c
+++ rendu4/vbc/vbc2/vbc.c
@@ -1,4 +1,6 @@
+//This file is given at the exam
 #include "vbc.h"
+
 
 node    *new_node(node n)
 {
@@ -8,6 +10,7 @@ node    *new_node(node n)
     *ret = n;
     return (ret);
 }
+
 
 void    destroy_tree(node *n)
 {
@@ -47,6 +50,95 @@ int expect(char **s, char c)
     return (0);
 }
 
+//...
+node *parse_add(char **s)
+{
+    node *left = parse_mul(s);
+    if(!left)
+        return NULL;
+        while(accept(s, '+'))
+        {
+            node *right = parse_mul(s);
+            if(!right)
+            {
+                destroy_tree(left);
+                return NULL;
+            }
+            node *tmp = new_node((node){.type = ADD, .l = left, .r = right});                                                                                                  
+            if (!tmp)                                                                                                                                                            
+            {
+                destroy_tree(left);                                                                                                                                              
+                destroy_tree(right);                                                                                                                                           
+                return NULL;
+            }
+            left = tmp;
+    }
+    return left;
+}
+node *parse_mul(char **s)
+{
+    node *left = parse_factor(s);
+    if(!left)
+        return NULL;
+    while(accept(s, '*'))
+    {
+        node *right = parse_factor(s);
+        if(!right)
+        {
+            destroy_tree(left);
+            return NULL;
+        }
+        node *tmp = new_node((node){.type = MULTI, .l = left, .r = right});                                                                                                  
+        if (!tmp)                                                                                                                                                            
+        {
+            destroy_tree(left);                                                                                                                                              
+            destroy_tree(right);                                                                                                                                           
+            return NULL;
+        }
+        left = tmp;
+    }
+    return left;
+}
+node *parse_factor(char **s)
+{
+    
+    if(isdigit(**s))
+    {
+        int val = **s - '0';
+        (*s)++;
+        return (new_node((node){.type = VAL, .val = val}));
+    }
+    if(**s == '(')
+    {
+        (*s)++;
+        node *n = parse_add(s);
+        if (!n) return NULL;
+        if(!expect(s, ')'))
+        {
+            destroy_tree(n);
+            return NULL;
+        }
+        return n;
+    }
+    unexpected(**s);
+    return NULL;
+
+}
+
+
+node    *parse_expr(char *s)
+{
+    node *ret = parse_add(&s);
+
+    if (*s) 
+    {
+        destroy_tree(ret);
+        unexpected(*s);
+        return (NULL);
+    }
+    return (ret);
+}
+
 int eval_tree(node *tree)
 {
     switch (tree->type)
@@ -58,32 +150,15 @@ int eval_tree(node *tree)
         case VAL:
             return (tree->val);
     }
-    return 0; //nunca debería llegar aquí
 }
 
 int main(int argc, char **argv)
 {
     if (argc != 2)
         return (1);
-    
-    // ✅ CORRECCIÓN: Usar variable auxiliar para el puntero
-    char *input = argv[1];
-    node *tree = parse_expr(&input);
-    
+    node *tree = parse_expr(argv[1]);
     if (!tree)
         return (1);
-    
-    // ✅ VALIDACIÓN DEL FINAL: Debe estar AQUÍ, no en parse_expr
-    if (*input)
-    {
-        unexpected(*input);
-        destroy_tree(tree);
-        return (1);
-    }
-    
     printf("%d\n", eval_tree(tree));
     destroy_tree(tree);
-    
-    return (0);
 }
-
--- rendu4/vbc/vbc2/vbc.h
+++ rendu4/vbc/vbc2/vbc.h
@@ -1,5 +1,5 @@
 #ifndef VBC_H
-#define VBC_H
+# define VBC_H
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -16,14 +16,9 @@ typedef struct node {
     struct node *r;
 }   node;
 
-node *parse_expr(char **s);
-node *parse_factor(char **s);
-node *parse_val(char **s);
-
-int expect(char **s, char c);
-int accept(char **s, char c);
-void    unexpected(char c);
-void    destroy_tree(node *n);
-node    *new_node(node n);
+node *parse_factor(char **s);                                                                         
+node *parse_mul(char **s);                                                                            
+node *parse_add(char **s);                                                                          
+node *parse_expr(char *s);  
 
 #endif
